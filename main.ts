b39f82ef8f23a05b8946c612e9e1d5408ba7cdce
AlexTheManMan <alexey.kozub.al@gmail.com>
2025-05-08 16:44:18 +1000

    Support hue and brightness

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
+    showHue(hue)
+})
+function showHue (hue: number) {
+    tileDisplay.setBrightness(bright)
+    tileDisplay.showRainbow(hue, hue)
+}
+input.onButtonPressed(Button.B, function () {
     showRainbow()
 })
 radio.onReceivedValue(function (name, value) {
@@ -7,16 +14,23 @@ radio.onReceivedValue(function (name, value) {
         bright = value
         showRainbow()
     }
+    if (name == "hue") {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+        hue = value
+        showHue(hue)
+    }
 })
 function showRainbow () {
     tileDisplay.setBrightness(bright)
     tileDisplay.showRainbow(1, 360)
 }
 let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
+let hue = 0
 let bright = 0
 music.setVolume(16)
 radio.setGroup(1)
 bright = 16
+hue = 180
 tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 basic.forever(function () {
 	
