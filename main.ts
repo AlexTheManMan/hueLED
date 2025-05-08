bd03c820c2af75225ea008e47c3f5f52d195a7ce
AlexTheManMan <alexey.kozub.al@gmail.com>
2025-05-08 19:59:46 +1000

    Start experimenting with accelerometer

--- main.ts
+++ main.ts
@@ -23,18 +23,21 @@ radio.onReceivedValue(function (name, value) {
 function showRainbow () {
     tileDisplay.setBrightness(bright)
     tileDisplay.showRainbow(1, 360)
-    basic.pause(500)
-    tileDisplay.rotate(50)
-    tileDisplay.show()
 }
 let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
 let hue = 0
 let bright = 0
+let prevHeight = 0
+let height = 0
+let diff = 0
 music.setVolume(16)
 radio.setGroup(1)
 bright = 16
 hue = 180
 tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+tileDisplay.setBrightness(bright)
 basic.forever(function () {
-	
+    if (input.logoIsPressed()) {
+        height = input.acceleration(Dimension.Z)
+    }
 })
