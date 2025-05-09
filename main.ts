b6008a9d043be04fd42b117f847a8ed7f7dfc6b0
AlexTheManMan <alexey.kozub.al@gmail.com>
2025-05-09 16:26:52 +1000

    Z acceleration function changes green and orange

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ function showHue (hue: number) {
 input.onButtonPressed(Button.B, function () {
     showRainbow()
 })
+input.onGesture(Gesture.Shake, function () {
+	
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "bright") {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
@@ -24,11 +27,11 @@ function showRainbow () {
     tileDisplay.setBrightness(bright)
     tileDisplay.showRainbow(1, 360)
 }
+let height = 0
 let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
 let hue = 0
 let bright = 0
 let prevHeight = 0
-let height = 0
 let diff = 0
 music.setVolume(16)
 radio.setGroup(1)
@@ -38,6 +41,12 @@ tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBi
 tileDisplay.setBrightness(bright)
 basic.forever(function () {
     if (input.logoIsPressed()) {
-        height = input.acceleration(Dimension.Z)
+        height = Math.round(input.acceleration(Dimension.Z) / 100)
+        if (height <= 9) {
+            tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.Green))
+        }
+        if (height >= 11) {
+            tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.Orange))
+        }
     }
 })
